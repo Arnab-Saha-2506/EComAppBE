addcd5d2f17dd2e115a05e73536dbdce25829e74
Arnab Saha <arnabsaha2506@gmail.com>
2025-12-28 11:59:26 +0530

    Adding all files first

---
+++ src/main/java/com/example/myEcomAppBE/MyEcomAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.myEcomAppBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEcomAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEcomAppBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myEcomAppBE/MyEcomAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myEcomAppBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEcomAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
