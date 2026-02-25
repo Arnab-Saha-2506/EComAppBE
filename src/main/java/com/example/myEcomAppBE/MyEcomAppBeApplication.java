41ff9b99824ee7a3d70a7a83e3ee774ddf78efc0
Arnab Saha <arnabsaha2506@gmail.com>
2026-02-25 14:09:48 +0530

    Added Eureka integration

--- src/main/java/com/example/myEcomAppBE/MyEcomAppBeApplication.java
+++ src/main/java/com/example/myEcomAppBE/MyEcomAppBeApplication.java
@@ -3,10 +3,12 @@ package com.example.myEcomAppBE;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class MyEcomAppBeApplication {
 
 	public static void main(String[] args) {
